58158c75d0cebb7a575ab93ccd2f88e11587e4dd
JunKyus <34362701+kuk941025@users.noreply.github.com>
2018-10-11 01:32:55 +0900

    listview + butterknife added

--- app/src/main/java/skku/fit4you_android/TestMainActivity.java
+++ app/src/main/java/skku/fit4you_android/TestMainActivity.java
@@ -1,13 +1,42 @@
 package skku.fit4you_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import skku.fit4you_android.Util.Constants;
 
 public class TestMainActivity extends AppCompatActivity {
-
+    @BindView(R.id.test_list_view)
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_main);
+
+        ButterKnife.bind(this);
+        ArrayList<String> activities = new ArrayList<>();
+        for (Class c : Constants.Activities)
+            activities.add(c.getSimpleName());
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, activities);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(TestMainActivity.this, Constants.Activities[position]);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/skku/fit4you_android/Util/Constants.java
@@ -0,0 +1,7 @@
+package skku.fit4you_android.Util;
+
+import skku.fit4you_android.Activity.MainActivity;
+
+public class Constants {
+    public static final Class[] Activities = {MainActivity.class};
+}
