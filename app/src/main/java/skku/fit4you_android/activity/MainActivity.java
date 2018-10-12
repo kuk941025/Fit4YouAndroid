99cffb68a7c61c003eb0c08053ce52088d7762a1
JunKyus <34362701+kuk941025@users.noreply.github.com>
2018-10-13 01:03:42 +0900

    toolbar layout for each tab

--- app/src/main/java/skku/fit4you_android/activity/MainActivity.java
+++ app/src/main/java/skku/fit4you_android/activity/MainActivity.java
@@ -6,8 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,7 +26,14 @@ public class MainActivity extends AppCompatActivity {
     TextView toolTitle;
     @BindView(R.id.toolbar_main)
     Toolbar toolbar;
+    @BindView(R.id.toolbar_fit_room_layout)
+    View toolFitLayout;
+    @BindView(R.id.toolbar_home_layout)
+    View toolHomeLayout;
+    @BindView(R.id.toolbar_news_layout)
+    View toolNewsLayout;
 
+    private ArrayList<View> viewToolbars;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,12 +44,21 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(pageAdapter);
 
         viewPager.addOnPageChangeListener(pageChangeListener);
-        setSupportActionBar(toolbar);
         tabLayout.setupWithViewPager(viewPager);
-        toolTitle.setText(tabLayout.getTabAt(0).getText());
+        setToolbar();
+
 
     }
 
+    private void setToolbar(){
+        setSupportActionBar(toolbar);
+        viewToolbars = new ArrayList<>();
+        viewToolbars.add(toolFitLayout);
+        viewToolbars.add(toolHomeLayout);
+        viewToolbars.add(toolNewsLayout);
+        toolTitle.setText(tabLayout.getTabAt(0).getText());
+        viewToolbars.get(0).setVisibility(View.VISIBLE);
+    }
     private ViewPager.OnPageChangeListener pageChangeListener = new ViewPager.OnPageChangeListener() {
         @Override
         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -50,7 +69,9 @@ public class MainActivity extends AppCompatActivity {
         public void onPageSelected(int position) {
             Log.d("Fit selected: ", position + ".");
             toolTitle.setText(tabLayout.getTabAt(position).getText());
-
+            for (View v : viewToolbars)
+                v.setVisibility(View.GONE);
+            if (viewToolbars.size() > position) viewToolbars.get(position).setVisibility(View.VISIBLE);
         }
 
         @Override
