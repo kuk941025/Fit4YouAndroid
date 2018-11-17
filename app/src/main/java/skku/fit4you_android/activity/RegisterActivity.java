82f1b758cd7c3220ee844bd54f8a0342aba9339a
JunKyus <kuk941025@gmail.com>
2018-11-17 20:27:56 +0900

    activites added

---
+++ app/src/main/java/skku/fit4you_android/activity/RegisterActivity.java
@@ -0,0 +1,15 @@
+package skku.fit4you_android.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import skku.fit4you_android.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
--- app/src/main/java/skku/fit4you_android/util/Constants.java
+++ app/src/main/java/skku/fit4you_android/util/Constants.java
@@ -1,10 +1,13 @@
 package skku.fit4you_android.util;
 
+import skku.fit4you_android.activity.FollowingManagementActivity;
 import skku.fit4you_android.activity.LoginActivity;
 import skku.fit4you_android.activity.MainActivity;
+import skku.fit4you_android.activity.RegisterActivity;
+import skku.fit4you_android.activity.UploadClothingActivity;
 
 public class Constants {
-    public static final Class[] Activities = {MainActivity.class, LoginActivity.class};
+    public static final Class[] Activities = {MainActivity.class, LoginActivity.class, UploadClothingActivity.class, RegisterActivity.class, FollowingManagementActivity.class};
     public static final String[] TabNames = {"Fit Room", "Home", "My Page", "Setting"};
     public static final String[] HomeShowOptions = {"Latest", "Most Liked", "Most Views"};
     public static final String[] HomeFilter = {"Only Following", "20s Popular", "30s Popular"};
