c1535df7b783cccc662f6191ff962b017e178db3
JunKyus <kuk941025@gmail.com>
2018-11-18 02:15:02 +0900

    keyboard hidden when activity starts

--- app/src/main/java/skku/fit4you_android/activity/DetailPostActivity.java
+++ app/src/main/java/skku/fit4you_android/activity/DetailPostActivity.java
@@ -2,6 +2,7 @@ package skku.fit4you_android.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import skku.fit4you_android.R;
 
@@ -11,5 +12,11 @@ public class DetailPostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_post);
+
+        hide_keyboard();
+    }
+
+    private void hide_keyboard(){
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
 }
