76cfcfa4b649c2674c4f4a3721fcf8273afa3d42
JunKyus <kuk941025@gmail.com>
2018-11-18 01:40:56 +0900

    detail post layout update

--- app/src/main/java/skku/fit4you_android/util/Constants.java
+++ app/src/main/java/skku/fit4you_android/util/Constants.java
@@ -1,5 +1,6 @@
 package skku.fit4you_android.util;
 
+import skku.fit4you_android.activity.DetailPostActivity;
 import skku.fit4you_android.activity.FollowingManagementActivity;
 import skku.fit4you_android.activity.LoginActivity;
 import skku.fit4you_android.activity.MainActivity;
@@ -7,7 +8,8 @@ import skku.fit4you_android.activity.RegisterActivity;
 import skku.fit4you_android.activity.UploadClothingActivity;
 
 public class Constants {
-    public static final Class[] Activities = {MainActivity.class, LoginActivity.class, UploadClothingActivity.class, RegisterActivity.class, FollowingManagementActivity.class};
+    public static final Class[] Activities = {MainActivity.class, LoginActivity.class, UploadClothingActivity.class, RegisterActivity.class,
+            FollowingManagementActivity.class, DetailPostActivity.class};
     public static final String[] TabNames = {"Fit Room", "Home", "My Page", "Setting"};
     public static final String[] HomeShowOptions = {"Latest", "Most Liked", "Most Views"};
     public static final String[] HomeFilter = {"Only Following", "20s Popular", "30s Popular"};
