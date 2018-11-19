9e80d2067f4278cc1b107138251348269327d66f
JunKyus <34362701+kuk941025@users.noreply.github.com>
2018-11-20 00:25:11 +0900

    following management layouts added

--- app/src/main/java/skku/fit4you_android/activity/FollowingManagementActivity.java
+++ app/src/main/java/skku/fit4you_android/activity/FollowingManagementActivity.java
@@ -2,14 +2,31 @@ package skku.fit4you_android.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import skku.fit4you_android.R;
 
 public class FollowingManagementActivity extends AppCompatActivity {
-
+    @BindView(R.id.toolbar_main_icon)
+    ImageView imgToolIcon;
+    @BindView(R.id.following_toolbar)
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_following_management);
+
+        ButterKnife.bind(this);
+
+        setToolbar();
+    }
+
+    private void setToolbar(){
+        setSupportActionBar(toolbar);
+        imgToolIcon.setVisibility(View.VISIBLE);
     }
 }
---
+++ app/src/main/java/skku/fit4you_android/model/FollowingUser.java
@@ -0,0 +1,17 @@
+package skku.fit4you_android.model;
+
+public class FollowingUser {
+    private String userName;
+
+    public FollowingUser(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
