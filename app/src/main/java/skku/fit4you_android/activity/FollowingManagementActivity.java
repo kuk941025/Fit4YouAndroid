75774f23ff0c02b50b0eebeddc01a093ede50777
JunKyus <34362701+kuk941025@users.noreply.github.com>
2018-11-20 01:28:35 +0900

    expandable relative layout java code

--- app/src/main/java/skku/fit4you_android/activity/FollowingManagementActivity.java
+++ app/src/main/java/skku/fit4you_android/activity/FollowingManagementActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,6 +18,11 @@ public class FollowingManagementActivity extends AppCompatActivity {
     ImageView imgToolIcon;
     @BindView(R.id.following_toolbar)
     Toolbar toolbar;
+    @BindView(R.id.following_layout_followers)
+    View viewFollowers;
+    @BindView(R.id.following_layout_followings)
+    View viewFollowings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +31,24 @@ public class FollowingManagementActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setToolbar();
+        setExpandableLayout(viewFollowers, "100 Followers");
+        setExpandableLayout(viewFollowings, "100 Followings");
     }
 
+    private void setExpandableLayout(View view, String text){
+        TextView txtTitle = view.findViewById(R.id.template_following_txt_tpye);
+        txtTitle.setText(text);
+
+        final View layout = view.findViewById(R.id.template_following_layout_top);
+        final ExpandableRelativeLayout expandableRelativeLayout = view.findViewById(R.id.template_following_layout_expandable);
+        expandableRelativeLayout.collapse();
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                expandableRelativeLayout.toggle();
+            }
+        });
+    }
     private void setToolbar(){
         setSupportActionBar(toolbar);
         imgToolIcon.setVisibility(View.VISIBLE);
