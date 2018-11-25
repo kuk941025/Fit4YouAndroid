34db8876b0fbc9785555b9c5dab50d4b6abe0e20
JunKyus <34362701+kuk941025@users.noreply.github.com>
2018-11-26 02:08:54 +0900

    opencv test activity included

--- app/src/main/java/skku/fit4you_android/util/Constants.java
+++ app/src/main/java/skku/fit4you_android/util/Constants.java
@@ -4,12 +4,13 @@ import skku.fit4you_android.activity.DetailPostActivity;
 import skku.fit4you_android.activity.FollowingManagementActivity;
 import skku.fit4you_android.activity.LoginActivity;
 import skku.fit4you_android.activity.MainActivity;
+import skku.fit4you_android.activity.OpenCVTest;
 import skku.fit4you_android.activity.RegisterActivity;
 import skku.fit4you_android.activity.UploadClothingActivity;
 
 public class Constants {
     public static final Class[] Activities = {MainActivity.class, LoginActivity.class, UploadClothingActivity.class, RegisterActivity.class,
-            FollowingManagementActivity.class, DetailPostActivity.class};
+            FollowingManagementActivity.class, DetailPostActivity.class, OpenCVTest.class};
     public static final String[] TabNames = {"Fit Room", "Home", "My Page", "Setting"};
     public static final String[] HomeShowOptions = {"Latest", "Most Liked", "Most Views"};
     public static final String[] HomeFilter = {"Only Following", "20s Popular", "30s Popular"};
