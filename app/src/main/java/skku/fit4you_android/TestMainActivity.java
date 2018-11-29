8630dcfaf354ea496eb6bd8e162bdc244004bc0b
JunKyus <34362701+kuk941025@users.noreply.github.com>
2018-11-30 00:12:32 +0900

    permission check

--- app/src/main/java/skku/fit4you_android/TestMainActivity.java
+++ app/src/main/java/skku/fit4you_android/TestMainActivity.java
@@ -1,6 +1,13 @@
 package skku.fit4you_android;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +24,8 @@ import skku.fit4you_android.util.Constants;
 public class TestMainActivity extends AppCompatActivity {
     @BindView(R.id.test_list_view)
     ListView listView;
+
+    private final int PERMISSION_CODE = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,5 +47,49 @@ public class TestMainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        tryPermCheck();
+    }
+
+    private void tryPermCheck(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+
+        }
+        else{
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case PERMISSION_CODE:
+                if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[2] == PackageManager.PERMISSION_GRANTED){
+
+                }
+                else{
+                    AlertDialog.Builder alert = new AlertDialog.Builder(TestMainActivity.this);
+                    alert.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            finish();
+                        }
+                    });
+                    alert.setMessage("권한 설정이 필요합니다.");
+                    alert.show();
+                }
+                break;
+        }
     }
 }
