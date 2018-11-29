840d0bfdebc51054644eabf858061ca3aaf28c79
JunKyus <34362701+kuk941025@users.noreply.github.com>
2018-11-29 14:40:10 +0900

    register activity toolbar

--- app/src/main/java/skku/fit4you_android/activity/RegisterActivity.java
+++ app/src/main/java/skku/fit4you_android/activity/RegisterActivity.java
@@ -2,14 +2,35 @@ package skku.fit4you_android.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import skku.fit4you_android.R;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    @BindView(R.id.register_toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.toolbar_main_title)
+    TextView toolTitle;
+    @BindView(R.id.toolbar_main_icon)
+    ImageView toolImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        ButterKnife.bind(this);
+        setToolbar();
+    }
+
+    private void setToolbar(){
+        setSupportActionBar(toolbar);
+        toolTitle.setText("Register");
+        toolImage.setVisibility(View.VISIBLE);
+        toolImage.setImageDrawable(getResources().getDrawable(R.drawable.img_back));
     }
 }
