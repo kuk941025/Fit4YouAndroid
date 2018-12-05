63e37b337203d4dc8edd812cf17e8dd6c2f09b1f
JunKyus <34362701+kuk941025@users.noreply.github.com>
2018-12-06 00:06:47 +0900

    crawling test activity added

---
+++ app/src/main/java/skku/fit4you_android/activity/CrawlingTestActivity.java
@@ -0,0 +1,13 @@
+package skku.fit4you_android.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrawlingTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crawling_test);
+    }
+}
