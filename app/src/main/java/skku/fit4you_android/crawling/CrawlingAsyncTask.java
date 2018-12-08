95afab8321be65d4a9784a570322af8c4d287140
LIM Dongjun <flamecracker1220@gmail.com>
2018-12-08 15:48:43 +0900

    주석들 추가

--- app/src/main/java/skku/fit4you_android/crawling/CrawlingAsyncTask.java
+++ app/src/main/java/skku/fit4you_android/crawling/CrawlingAsyncTask.java
@@ -32,34 +32,33 @@ public class CrawlingAsyncTask extends AsyncTask<String,Integer,Clothing> {
             // String url5 = "https://store.musinsa.com/app/product/detail/900297/0"; // Outer
             String url6 = "https://store.musinsa.com/app/product/detail/67485/0"; // bottom
             Document doc = Jsoup.connect(url6).get();
-            Elements eTitle = doc.select("span.product_title span");
-            Elements eCost = doc.select("span.product_article_price");
-            Elements type = doc.select("p.item_categories a[href]");
-            Elements size = doc.select("table.table_th_grey tbody th");
-            Elements nsize = doc.select("table.table_th_grey tbody td.goods_size_val");
+            Elements eTitle = doc.select("span.product_title span");// 제목
+            Elements eCost = doc.select("span.product_article_price"); // 코스트
+            Elements type = doc.select("p.item_categories a[href]"); // 타입
+            Elements size = doc.select("table.table_th_grey tbody th"); // 한 옷의 사이즈 종류 갯수
+            Elements nsize = doc.select("table.table_th_grey tbody td.goods_size_val");// 실제 사이즈?
             Elements img = doc.select("div.product_img_basic img");
 
-            String title = eTitle.get(0).text();
-            String cost = eCost.get(eCost.size()-1).text();
-            String src = "https://" + img.attr("src");
+            String title = eTitle.get(0).text(); // 제목
+            String cost = eCost.get(eCost.size()-1).text(); //비용
+            String src = "https://" + img.attr("src"); //주소
 
             String str = type.get(1).attr("href");
-            int clothType = (int)(str.charAt(str.length()-1)) - 48;
-            cloth = new Clothing(clothType, title, cost, src);
+            int clothType = (int)(str.charAt(str.length()-1)) - 48; //타입 1과2 는 상의 3은 하의
+            cloth = new Clothing(clothType, title, cost, src); // 제목 비용 주소 타입으로 초기화
             result += "title: "+cloth.title+"\nCost: "+cloth.cost+"\nClothType: "+cloth.clothType;
-            if (clothType == 1 || clothType == 2) {
+            if (clothType == 1 || clothType == 2) { //상의일 경우
                 result+="\nTop Cloth";
-                cloth.topSize = new ArrayList<TopSize>();
-                cloth.bottomSize = null;
+                cloth.topSize = new ArrayList<TopSize>(); //cloth의 탑사이즈 배열 초기화
+                cloth.bottomSize = null;//바지사이즈는 안쓰니까
                 for(int i = 1; i < size.size(); i++) {
                     length = (int)Double.parseDouble(nsize.get(4*(i-1)).text());
                     shoulder = (int)Double.parseDouble(nsize.get(4*(i-1) + 1).text());
                     chest = (int)Double.parseDouble(nsize.get(4*(i-1) + 2).text());
                     sleeve = (int)Double.parseDouble(nsize.get(4*(i-1) + 3).text());
                     TopSize temp = new TopSize(size.get(i).text(),length,shoulder,chest,sleeve);
-                    cloth.topSize.add(temp);
+                    cloth.topSize.add(temp);//배열에 추가
                 }
-
                 if (size.size() != 0) {
                     for (int i = 0; i < cloth.topSize.size(); i++) {
                         result += "\ntype: "+cloth.topSize.get(i).type+"\nlength: "+cloth.topSize.get(i).length+"\nshoulder: "+cloth.topSize.get(i).shoulder+
@@ -67,14 +66,13 @@ public class CrawlingAsyncTask extends AsyncTask<String,Integer,Clothing> {
                     }
                 }
                 else {
-                    Log.d("crawling:","No Size Info.");
                     cloth.topSize = null;
                     cloth.bottomSize = null;
                 }
             }
             else {
-                cloth.topSize = null;
-                cloth.bottomSize = new ArrayList<BottomSize>();
+                cloth.topSize = null;// 상의 사이즈 필요없다.
+                cloth.bottomSize = new ArrayList<BottomSize>();//cloth 바텀 사이즈 배열 초기화
                 result+="\nBottom Cloth";
                 for(int i = 1; i < size.size(); i++) {
                     length = (int)Double.parseDouble(nsize.get(5*(i-1)).text());
@@ -83,26 +81,20 @@ public class CrawlingAsyncTask extends AsyncTask<String,Integer,Clothing> {
                     rise = (int)Double.parseDouble(nsize.get(5*(i-1) + 3).text());
                     hem = (int)Double.parseDouble(nsize.get(5*(i-1) + 4).text());
                     BottomSize temp = new BottomSize(size.get(i).text(),length,waist,thigh,rise,hem);
-                    cloth.bottomSize.add(temp);
+                    cloth.bottomSize.add(temp);//하의 사이즈 배열 추가
                 }
 
                 if (size.size() != 0) {
-
                     for (int i = 0; i < cloth.bottomSize.size(); i++) {
                         result += "\ntype: "+cloth.bottomSize.get(i).type+"\nlength: "+cloth.bottomSize.get(i).length+"\nwaist: "+cloth.bottomSize.get(i).waist+
                                 "\nthigh: "+cloth.bottomSize.get(i).thigh+"\nrise: "+cloth.bottomSize.get(i).rise+"\nhem: "+cloth.bottomSize.get(i).hem;
                     }
                 }
                 else {
-//                    Log.d("crawling:","No Size Info.");
                     cloth.topSize = null;
                     cloth.bottomSize = null;
                 }
             }
-
-//            Log.d("crawling:",cloth.url);
-//            result += "\nURL: "+cloth.url;
-
         } catch (Exception e) {
             Log.d("crawling:","No Size Info.");
             e.printStackTrace();
